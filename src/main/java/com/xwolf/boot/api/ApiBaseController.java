27a61a97a3a0d313069d5238ba60962051311e70
chenjianfei <chenjianfei@rongcapital.cn>
2018-03-21 11:06:30 +0800

    update

---
+++ src/main/java/com/xwolf/boot/api/ApiBaseController.java
@@ -0,0 +1,37 @@
+package com.xwolf.boot.api;
+
+import com.xwolf.boot.entity.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ * [Swagger解耦]
+ */
+public interface ApiBaseController {
+    @ApiOperation(value="获取用户列表", notes="")
+    public List<User> getUserList();
+
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    public String postUser(@RequestBody User user);
+
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer")
+    public User getUser(@PathVariable Integer id);
+
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
+    public String putUser(@PathVariable Integer id, @RequestBody User user);
+
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer")
+    public String deleteUser(@PathVariable Integer id);
+}
