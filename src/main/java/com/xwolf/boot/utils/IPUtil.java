637108f9af4a56fb5bdaa2a782e645055bc06278
xwolf <2324808561@qq.com>
2018-03-27 13:32:50 +0800

    IP tool

---
+++ src/main/java/com/xwolf/boot/utils/IPUtil.java
@@ -0,0 +1,43 @@
+package com.xwolf.boot.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * IP工具
+ * @author xwolf
+ * @since 1.8
+ */
+public class IPUtil {
+
+    /**
+     * 获取IP地址
+     * @param request
+     * @return
+     */
+    public static String getIP(HttpServletRequest request) {
+        if (request == null) {
+            return "unknown";
+        }
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Forwarded-For");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+
+
+}
